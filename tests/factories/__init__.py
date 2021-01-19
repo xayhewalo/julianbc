c8c18e3010332e049c62ef12d3b3cdb512a736b4
Guyrandy Jean-Gilles <gjgilles@tutanota.com>
2021-01-18 20:14:34 -0800

    rename 'model_factories' to 'db_factories'
    Signed-off-by: Guyrandy Jean-Gilles <gjgilles@tutanota.com>

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -1,11 +1,11 @@
 # flake8: noqa E401, F401, E501
-from .model_factories.eon_factories.customcalendar_factories import (
+from .db_factories.eon_factories.customcalendar_factories import (
     CalendarConversionFactory,
     ConvertibleCalendarFactory,
 )
 
 # fmt: off
-from .model_factories.eon_factories.customclock_factories import ConvertibleClockFactory  # noqa: E501
+from .db_factories.eon_factories.customclock_factories import ConvertibleClockFactory
 # fmt: on
 
 from .customtime_factories import ConvertibleTimeFactory
